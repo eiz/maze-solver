d73bd23f50092fa370ea3183ef03e889a8d17752
Mackenzie Straight <eiz@e7m.org>
2015-11-30 23:12:51 -0500

    Map parser, add some test maps

--- map.lisp
+++ map.lisp
@@ -6,15 +6,40 @@
    (start-point :accessor map-start-point)
    (end-point :accessor map-end-point)))
 
+(defun stream-lines (stream)
+  (loop for line = (read-line stream nil)
+       until (null line) collect line))
+
 (defun string-lines (string)
   (with-input-from-string (stream string)
-    (loop for line = (read-line stream nil)
-         until (null line) collect line)))
+    (stream-lines stream)))
 
-(defun parse-map (string)
-  (let* ((lines (string-lines string))
-         (height (length lines))
+(defun file-lines (path)
+  (with-open-file (stream path)
+    (stream-lines stream)))
+
+(defun symbol-class (char)
+  (case char
+    (#\# 'wall)
+    (#\S 'start)
+    (#\E 'end)
+    (otherwise 'floor)))
+
+(defun parse-map-lines (lines)
+  (let* ((height (length lines))
          (width (reduce #'max (mapcar #'length lines)))
-         (data (make-array (list height width))))
+         (data (make-array (list height width)))
+         (i 0))
+    (dolist (line lines)
+      (do ((j 0 (1+ j)))
+          ((= j (length line)))
+        (setf (aref data i j)
+              (symbol-class (aref line j))))
+      (incf i))
     (make-instance 'map :data data)))
 
+(defun parse-map-string (string)
+  (parse-map-lines (string-lines string)))
+
+(defun parse-map-file (path)
+  (parse-map-lines (file-lines path)))
