b50b33f1701090b369e21e008e9fc9072ea9c42c
Mackenzie Straight <eiz@e7m.org>
2015-12-01 01:25:37 -0500

    Basic A* implementation

--- astar.lisp
+++ astar.lisp
@@ -1,1 +1,97 @@
 (in-package :maze-solver)
+
+(defclass node ()
+  ((point :reader node-point
+          :initarg :point
+          :initform (error "Point is required."))
+   (best-cost :accessor node-best-cost :initform 'infinity)
+   (best-source :accessor node-best-source)
+   (original :accessor node-original :initarg :original)))
+
+(defun manhattan-distance (lhs rhs)
+  (+ (abs (- (car lhs) (car rhs)))
+     (abs (- (cdr lhs) (cdr rhs)))))
+
+(defun node-cost (node destination)
+  (if (eq (node-best-cost node) 'infinity)
+      most-positive-fixnum ; TODO
+      (+ (node-best-cost node)
+         (manhattan-distance (node-point node) destination))))
+
+(defun point-equals (lhs rhs)
+  (and (= (car lhs) (car rhs))
+       (= (cdr lhs) (cdr rhs))))
+
+(defun node-equals (lhs rhs)
+  (point-equals (node-point lhs) (node-point rhs)))
+
+(defun node-parent (nodes node)
+  (aref nodes (cdr (node-best-source node)) (car (node-best-source node))))
+
+(defun build-path (nodes source destination)
+  (let ((result nil))
+    (do ((current destination (node-parent nodes current)))
+        ((point-equals (node-point current) source))
+      (push (node-point current) result))
+    (cons source result)))
+
+(defun find-path (map source destination)
+  (let ((queue (make-instance 'priority-queue
+                              :comparer (lambda (l r)
+                                          (> (node-cost l destination)
+                                             (node-cost r destination)))))
+        (visited (make-hash-table :test #'eq))
+        (discovered (make-hash-table :test #'eq))
+        (nodes (make-array (list (map-height map) (map-width map))
+                           :initial-element nil)))
+    (loop for y from 0 below (map-height map) do
+         (loop for x from 0 below (map-width map) do
+              (when (not (eq (map-at map x y) 'wall))
+                (setf (aref nodes y x)
+                      (make-instance 'node :point (cons x y))
+                      (node-original (aref nodes y x)) (aref nodes y x)))))
+    (when (null (aref nodes (cdr destination) (car destination)))
+      (error "Destination must not be a wall"))
+    (when (null (aref nodes (cdr source) (car source)))
+      (error "Source must not be a wall"))
+    (let ((start-node (aref nodes (cdr source) (car source))))
+      (setf (node-best-cost start-node) 0)
+      (priority-queue-insert queue start-node)
+      (setf (gethash start-node discovered) t))
+    (priority-queue-insert queue (aref nodes (cdr source) (car source)))
+    (loop
+       (when (zerop (priority-queue-size queue))
+         (return nil))
+       (let* ((current (priority-queue-pop queue))
+              (current-x (car (node-point current)))
+              (current-y (cdr (node-point current))))
+         (when (point-equals (node-point current) destination)
+           (return (build-path nodes source current)))
+         (setf (gethash current visited) t)
+         (labels ((maybe-add-neighbor (x y)
+                    (let ((neighbor (aref nodes y x)))
+                      (when (and neighbor
+                                 (not (gethash (node-original neighbor)
+                                               visited)))
+                        (let ((new-cost (1+ (node-best-cost current))))
+                          (when (and (gethash (node-original neighbor)
+                                              discovered)
+                                     (< new-cost (node-best-cost neighbor)))
+                            (setf 
+                             neighbor (make-instance
+                                       'node
+                                       :point (cons x y)
+                                       :original (node-original neighbor))
+                             (aref nodes y x) neighbor))
+                          (when (eq (node-best-cost neighbor) 'infinity)
+                            (setf
+                             (node-best-cost neighbor) new-cost
+                             (node-best-source neighbor) (node-point current)
+                             (gethash (node-original neighbor) discovered) t)
+                            (priority-queue-insert queue neighbor)))))))
+           (maybe-add-neighbor current-x (1+ current-y))
+           (when (> current-y 0)
+            (maybe-add-neighbor current-x (1- current-y)))
+           (maybe-add-neighbor (1+ current-x) current-y)
+           (when (> current-x 0)
+            (maybe-add-neighbor (1- current-x) current-y)))))))
