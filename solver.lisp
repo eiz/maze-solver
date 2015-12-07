100f7bdef22f84f5ade7447ff9ed67996beebf62
Mackenzie Straight <eiz@e7m.org>
2015-12-06 23:44:30 -0500

    Add test that path finding for 'open' examines no unused nodes

--- solver.lisp
+++ solver.lisp
@@ -7,3 +7,9 @@
 (defun solve-maze-from-file (path)
   (let ((map (parse-map-file path)))
     (find-path map (map-start-point map) (map-end-point map))))
+
+(5am:test open-path-examines-optimal-node-count
+  (multiple-value-bind (path visited total-nodes)
+      (solve-maze-from-file (%test-case-path "open"))
+    (declare (ignore total-nodes))
+    (5am:is (= (length path) (length visited)))))
