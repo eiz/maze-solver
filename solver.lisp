e1c9d007980f9b590ee539f44186d640a19063c5
Mackenzie Straight <eiz@e7m.org>
2015-12-10 13:18:32 -0500

    Optimize draw-solution

--- solver.lisp
+++ solver.lisp
@@ -15,23 +15,24 @@
               (and draw (draw-solution map path visited-list))))))
 
 (defun draw-solution (map path visited-list)
-  (let* ((data (map-data map)))
+  (let* ((data (map-data map))
+         (visited-map (make-array
+                       (array-dimensions data) :initial-element nil)))
     (with-output-to-string (stream)
+      (loop for visited in visited-list do
+           (setf (aref visited-map (cdr visited) (car visited)) 'visited))
+      (loop for step in path do
+           (setf (aref visited-map (cdr step) (car step)) 'in-path))
       (loop for y from 0 below (map-height map) do
            (progn
              (loop for x from 0 below (map-width map) do
-                  (let ((char #\Space))
-                    (when (eq (aref data y x) 'wall)
-                      (setf char #\#))
-                    (when (find-if (lambda (point)
-                                     (and (= (car point) x) (= (cdr point) y)))
-                                   visited-list)
-                      (setf char #\?))
-                    (when (find-if (lambda (point)
-                                     (and (= (car point) x) (= (cdr point) y)))
-                                   path)
-                      (setf char #\x))
-                    (princ char stream)))
+                  (princ
+                   (cond
+                     ((eq (aref visited-map y x) 'in-path) #\x)
+                     ((eq (aref visited-map y x) 'visited) #\?)
+                     ((eq (aref data y x) 'wall) #\#)
+                     (t #\Space))
+                   stream))
              (princ #\Newline stream))))))
 
 (5am:test open-path-examines-optimal-node-count
